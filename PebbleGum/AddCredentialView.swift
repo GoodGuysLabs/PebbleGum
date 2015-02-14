50d77380f1311e63d3930b4e7f490d442c56a1b9
AkdM <akdm@me.com>
2015-02-14 16:51:51 +0100

    Credential view update

--- PebbleGum/AddCredentialView.swift
+++ PebbleGum/AddCredentialView.swift
@@ -13,7 +13,11 @@ class AddCredentialView: UIView, PBPebbleCentralDelegate, UITextFieldDelegate {
     var viewController: AddCredentialViewController? = nil
     let imageView:UIImageView?
     let functions = theFunctions()
-    var inputTitle:UITextField?
+    var displayLabel:UILabel!
+    var inputTitle:FloatLabelTextField?
+    var inputEmail:FloatLabelTextField?
+    var inputPassword:FloatLabelTextField?
+    var inputNotes:UITextView?
     
     override init(frame: CGRect)
     {
@@ -31,24 +35,93 @@ class AddCredentialView: UIView, PBPebbleCentralDelegate, UITextFieldDelegate {
         keychainNewIcon.text = String.fontAwesomeIconWithName("fa-key")
         self.addSubview(keychainNewIcon)
         
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "TextDidBeginEditing:", name: UITextFieldTextDidBeginEditingNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "TextDidEndEditing:", name: UITextFieldTextDidEndEditingNotification, object: nil)
+        
         // Credential Input Title
-        inputTitle = UITextField(frame: CGRectMake(frame.width - frame.width/1.5, frame.height/6, frame.width - frame.width/2.3, frame.height/14))
+        inputTitle = FloatLabelTextField(frame: CGRectMake(frame.width - frame.width/1.5, frame.height/6, frame.width - frame.width/2.3, frame.height/14))
+        inputTitle?.attributedPlaceholder = NSAttributedString(string: String(format: NSLocalizedString("AddCredentialInputTitle", comment: "")), attributes:[NSForegroundColorAttributeName: theFunctions().UIColorFromRGB("D1CCC0", alpha: 0.8)])
+        inputTitle?.titleActiveTextColour = theFunctions().UIColorFromRGB("FFFFFF", alpha: 0.5)
         inputTitle?.backgroundColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.12)
         inputTitle?.textColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.8)
         inputTitle?.leftView = UIView(frame: CGRectMake(0, 0, 5, 20))
         inputTitle?.leftViewMode = UITextFieldViewMode.Always
         inputTitle?.clearButtonMode = UITextFieldViewMode.WhileEditing
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "myMethod", name: UITextFieldTextDidBeginEditingNotification, object: nil)
-        //inputTitle?.becomeFirstResponder()
+        inputTitle?.keyboardAppearance = .Dark
         self.addSubview(inputTitle!)
+        
+        // Credential Input Email/Username
+        inputEmail = FloatLabelTextField(frame: CGRectMake(frame.width/14, frame.height/3, frame.width - frame.width/6, frame.height/14))
+        inputEmail?.attributedPlaceholder = NSAttributedString(string: String(format: NSLocalizedString("AddCredentialInputLogin", comment: "")), attributes:[NSForegroundColorAttributeName: theFunctions().UIColorFromRGB("D1CCC0", alpha: 0.8)])
+        inputEmail?.titleActiveTextColour = theFunctions().UIColorFromRGB("FFFFFF", alpha: 0.5)
+        inputEmail?.backgroundColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.12)
+        inputEmail?.textColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.8)
+        inputEmail?.leftView = UIView(frame: CGRectMake(0, 0, 5, 20))
+        inputEmail?.leftViewMode = UITextFieldViewMode.Always
+        inputEmail?.clearButtonMode = UITextFieldViewMode.WhileEditing
+        inputEmail?.autocapitalizationType = .None
+        inputEmail?.autocorrectionType = .No
+        inputEmail?.keyboardType = .EmailAddress
+        inputEmail?.keyboardAppearance = .Dark
+        self.addSubview(inputEmail!)
+        
+        // Credential Input Password
+        inputPassword = FloatLabelTextField(frame: CGRectMake(frame.width/14, frame.height/2.2, frame.width - frame.width/6, frame.height/14))
+        inputPassword?.attributedPlaceholder = NSAttributedString(string: String(format: NSLocalizedString("AddCredentialInputPassword", comment: "")), attributes:[NSForegroundColorAttributeName: theFunctions().UIColorFromRGB("D1CCC0", alpha: 0.8)])
+        inputPassword?.titleActiveTextColour = theFunctions().UIColorFromRGB("FFFFFF", alpha: 0.5)
+        inputPassword?.backgroundColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.12)
+        inputPassword?.textColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.8)
+        inputPassword?.leftView = UIView(frame: CGRectMake(0, 0, 5, 20))
+        inputPassword?.leftViewMode = UITextFieldViewMode.Always
+        inputPassword?.clearButtonMode = UITextFieldViewMode.WhileEditing
+        inputPassword?.autocapitalizationType = .None
+        inputPassword?.autocorrectionType = .No
+        inputPassword?.keyboardAppearance = .Dark
+        inputPassword?.secureTextEntry = true
+        inputPassword?.tag = 3
+        self.addSubview(inputPassword!)
+        
+        // Credential Input Notes
+        inputNotes = UITextView(frame: CGRectMake(frame.width/14, frame.height/1.65, frame.width - frame.width/6, frame.height/7))
+        inputNotes?.backgroundColor = theFunctions().UIColorFromRGB("FFFFFF", alpha: 0.12)
+        inputNotes?.textColor = theFunctions().UIColorFromRGB("FFFFFF", alpha: 0.8)
+        inputNotes?.keyboardAppearance = .Dark
+        inputNotes?.tag = 4
+        self.addSubview(inputNotes!)
+    }
+
+    func TextDidBeginEditing(sender: AnyObject) {
+
+        let theTextField:FloatLabelTextField = sender.object! as FloatLabelTextField
+        let thePlaceholder = theTextField.placeholder
+        
+        theTextField.backgroundColor = theFunctions().UIColorFromRGB("FFFFFF", alpha: 0.5)
+        theTextField.textColor = theFunctions().UIColorFromRGB("46474f")
+        theTextField.attributedPlaceholder = NSAttributedString(string: thePlaceholder!, attributes:[NSForegroundColorAttributeName: theFunctions().UIColorFromRGB("434553", alpha: 0.8)])
+        theTextField.titleActiveTextColour = theFunctions().UIColorFromRGB("434553", alpha: 0.5)
+
+        if (theTextField.tag == 3) {
+            theTextField.secureTextEntry = false
+        }
         
     }
     
-    func myMethod(notification:NSNotification) {
-        println(":")
-        println(notification)
+    func TextDidEndEditing(sender: AnyObject) {
+        
+        let theTextField:FloatLabelTextField = sender.object! as FloatLabelTextField
+        let thePlaceholder = theTextField.placeholder
+        
+        theTextField.backgroundColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.12)
+        theTextField.textColor = functions.UIColorFromRGB("FFFFFF", alpha: 0.8)
+        theTextField.attributedPlaceholder = NSAttributedString(string: thePlaceholder!, attributes:[NSForegroundColorAttributeName: theFunctions().UIColorFromRGB("D1CCC0", alpha: 0.8)])
+        theTextField.titleActiveTextColour = theFunctions().UIColorFromRGB("FFFFFF", alpha: 0.5)
+        
+        if (theTextField.tag == 3) {
+            theTextField.secureTextEntry = true
+        }
+        
     }
-    
+
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         self.endEditing(true)
     }
